3bb79fa3586885944006850bc480f20a368f17ae
shegsnya <shagunarya1110@gmail.com>
2025-06-06 01:06:20 +0530

    added user login control

--- brainbop_project/urls.py
+++ brainbop_project/urls.py
@@ -3,12 +3,16 @@ from django.urls import path,include
 from quiz import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quiz.urls')), 
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home, name='home'), 
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
